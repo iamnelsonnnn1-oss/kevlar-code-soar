2e54c5faa6d999a923715cc55135708ba38ac15d
iamnelsonnnn1-oss <i.am.nelsonnnn1@gmail.com>
2026-03-15 16:24:26 +0100

    Kevlar-Code-SOAR: Finalize dynamic infrastructure blocks and variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,11 +1,16 @@
 variable "aws_region" {
-  description = "The AWS region to deploy the Kevlar-Core"
+  description = "AWS region for deployment"
   type        = string
   default     = "eu-central-1"
 }
 
 variable "instance_type" {
-  description = "Instance size for ELK/SOAR workloads"
+  description = "EC2 instance type"
   type        = string
   default     = "t3.small"
+}
+
+variable "ec2_ami" {
+  description = "AMI ID for the Ubuntu server"
+  type        = string
 }
